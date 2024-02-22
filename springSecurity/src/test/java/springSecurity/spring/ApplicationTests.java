23487b8939e55e0323d5018ce36d00969ed1c4c2
최민석 <minseok@choeminseog-ui-MacBookPro.local>
2024-02-22 12:12:22 +0900

    feat: Initialize

---
+++ springSecurity/src/test/java/springSecurity/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package springSecurity.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
